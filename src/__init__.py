6df0974a9b12adbcf581f0797b08291080ffd896
Matthias Wolff <m_wolf37@uni-muenster.de>
2025-02-27 17:00:38 +0100

    Added dependencies

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from cvkan.models.CVKAN import CVKAN
-from cvkan.models.wrapper.WrapperTemplate import WrapperTemplate
-from cvkan.models.wrapper.CVKANWrapper import CVKANWrapper
-from cvkan.utils.explain_kan import KANExplainer
-from cvkan.utils.plotting.plot_kan import KANPlot
-
-__all__ = [CVKAN, CVKANWrapper, KANExplainer, KANPlot]
