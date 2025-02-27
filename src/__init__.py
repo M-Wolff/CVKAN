f2344f626594405d85c8f858adcd7367965a0287
Matthias Wolff <m_wolf37@uni-muenster.de>
2025-02-27 16:17:34 +0100

    Added __init__.py files

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+from cvkan.models.CVKAN import CVKAN
+from cvkan.models.wrapper.WrapperTemplate import WrapperTemplate
+from cvkan.models.wrapper.CVKANWrapper import CVKANWrapper
+from cvkan.utils.explain_kan import KANExplainer
+from cvkan.utils.plotting.plot_kan import KANPlot
+
+__all__ = [CVKAN, CVKANWrapper, KANExplainer, KANPlot]
